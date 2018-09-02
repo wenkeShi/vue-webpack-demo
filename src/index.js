539e363eb1ee86e0ed3b76f22e8450ff969c163c
wkshi <wkshi@finbtc.net>
2018-09-02 23:19:52 +0800

    modified index page

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ let sc = document.createElement('img')
 i.addEventListener('change', function(e){
   sc.src =  require('./style/' + 'ercode'+e.target.value+'.jpg')
 })
+
 document.body.appendChild(i)
 document.body.appendChild(sc)
 // setTimeout(()=>{
