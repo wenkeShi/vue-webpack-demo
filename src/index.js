fe7353023b0cb94be81104791b2654d118f5a068
wkshi <wkshi@finbtc.net>
2019-02-10 23:17:23 +0800

    modified index.js

--- src/index.js
+++ src/index.js
@@ -22,7 +22,6 @@ console.log(test)
 showTest()
 // test = 2
 console.log(p)
-
 window.test = function(){console.log(test)}
 window.testP = function(){console.log(p)}
 // let i = document.createElement('input')
